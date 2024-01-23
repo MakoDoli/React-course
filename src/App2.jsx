b02f509a73200ce18a4b94550a97a13316f8f7ae
MakoDoli <makodolidze@hotmail.com>
2024-01-23 14:54:58 +0400

    event handlers and propagation

--- src/App2.jsx
+++ src/App2.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 import Gallery from "./gallery";
 import List from "./List";
 import Teaset from "./cup";
+import Sculptures from "./Sculptures";
 
 function App2() {
   return (
     <div>
+      <Sculptures />
       <Gallery />
       <Teaset />
       <List />
--- src/history.jsx
+++ src/history.jsx
@@ -75,3 +75,38 @@ function NewBtn() {
 //---------------------------
 //----------------------------
 // useDebugValue - will display value in dev tools component
+//-----------------------------
+//---------------------------
+
+//**************************** *********************/
+//          JSX
+
+// If we need to return several jsx element in each map() item, we wrap them in <Fragment>to pass key
+
+/*import { Fragment } from 'react';
+
+// ...
+
+const listItems = people.map(person =>
+  <Fragment key={person.id}>
+    <h1>{person.name}</h1>
+    <p>{person.bio}</p>
+  </Fragment>
+);*/
+//*************************************** */
+// React components must be pure functions
+// Event handlers (onClick) propagate events up to the parent. both child and parent event handler will be triggered
+// use e.stopPropagation() to prevent
+// onClickCapture runs code on 'capture' phase, which means code first runs in parent element. as React doesnt support 'capture' phase unlike traditional DOM bubling
+//********************************* */
+///   EVENT   handlers
+
+/* You can handle events by passing a function as a prop to an element like <button>.
+Event handlers must be passed, not called! onClick={handleClick}, not onClick={handleClick()}.
+You can define an event handler function separately or inline.
+Event handlers are defined inside a component, so they can access props.
+You can declare an event handler in a parent and pass it as a prop to a child.
+You can define your own event handler props with application-specific names.
+Events propagate upwards. Call e.stopPropagation() on the first argument to prevent that.
+Events may have unwanted default browser behavior. Call e.preventDefault() to prevent that.
+Explicitly calling an event handler prop from a child handler is a good alternative to propagation.*/
