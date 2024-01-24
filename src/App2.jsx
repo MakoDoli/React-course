d3a996fb6c328aec1f6f8e46b734da42e6113217
MakoDoli <makodolidze@hotmail.com>
2024-01-24 17:38:07 +0400

    update array in state

--- src/App2.jsx
+++ src/App2.jsx
@@ -4,10 +4,12 @@ import List from "./List";
 import Teaset from "./cup";
 import Sculptures from "./Sculptures";
 import Form from "./Form";
+import ShoppingList from "./ShoppingLIst";
 
 function App2() {
   return (
     <div>
+      <ShoppingList />
       <Form />
       <Sculptures />
       <Gallery />
