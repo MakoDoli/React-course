7f131e97c20a741407c4cb0d2ba34edee5d75387
MakoDoli <makodolidze@hotmail.com>
2024-01-27 20:59:00 +0400

    intermediate commit

--- src/homework/ButtonsSum.jsx
+++ src/homework/ButtonsSum.jsx
@@ -2,12 +2,14 @@
 // create many buttons, increase their value by one onclick, (later sum up all button values in context)
 // create states in each button
 // make onClick handler increasing count
-// create context object with sum and setsum
+// create context object with sum ,setsum and reset/setReset -boolean
 // create context provider and wrap buttons container div
 // pass value object containting state of 'sum' and 'setSum', thtough provider
 // import context through useContext hook in button component
 // add useContext()'s setSum() to button onClick handler so it updates provider's 'sum'
 //
+// create reset button and add onClick listener setReset to change context's reset
+//add useEffect to buttons to set count to 0 when context's reset changes
 
 //import { useContext } from "react";
 import Button from "./Button";
--- src/homework/SumContext.jsx
+++ src/homework/SumContext.jsx
@@ -1,12 +1,17 @@
 import { createContext, useState } from "react";
 
-export const SumContext = createContext({ sum: 0, setSum: () => {} });
+export const SumContext = createContext({
+  sum: 0,
+  setSum: () => {},
+  reset: false,
+  setReset: () => {},
+});
 
 // eslint-disable-next-line react/prop-types
 export const SumProvider = ({ children }) => {
-  const [sum, setSum] = useState(0);
+  const [sum, setSum, reset, setReset] = useState(0);
   return (
-    <SumContext.Provider value={{ sum, setSum }}>
+    <SumContext.Provider value={{ sum, setSum, reset, setReset }}>
       {children}
     </SumContext.Provider>
   );
