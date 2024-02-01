e064b5b17fe064ecf34019fe8df725d8d8503769
MakoDoli <makodolidze@hotmail.com>
2024-02-01 16:28:27 +0400

    create carousel with useRef

--- src/App2.jsx
+++ src/App2.jsx
@@ -9,6 +9,7 @@ import ChatContacts from "./ChatContact";
 import VideoPlayer from "./VideoPlayer";*/
 import LifeCycle from "./intermediate/LifeCycle";
 import ButtonSum from "./homework/ButtonsSum";
+import CatFriends from "./intermediate/hooks/useRefCarousel";
 //import UseHook from "./intermediate/hooks/useRef";
 //import Accordion from "./intermediate/managestate/Accordion";
 //import StatusUpdate from "./intermediate/managestate/StatusUpdate";
@@ -20,7 +21,6 @@ function App2() {
     <div>
       <ButtonSum />
       <hr></hr>
-
       <LifeCycle />
       {/* <UseHook />
       <Accordion />
--- src/intermediate/hooks/useRef.jsx
+++ src/intermediate/hooks/useRef.jsx
@@ -1,9 +1,27 @@
-import { useState, useRef } from "react";
+import { useState, useRef, forwardRef, useImperativeHandle } from "react";
+//************************ */
+
+// DO NOT MODIFY DOM controlled by react state
+
+// passing refto nested component, way to access child's node from parent with ref
+
+// eslint-disable-next-line react/display-name
+const NestedComponent = forwardRef((ref) => {
+  const realRef = useRef(null);
+  useImperativeHandle(ref, () => ({
+    focus() {
+      realRef.current.focus();
+    },
+  }));
+  return <input ref={ref} />;
+});
 
 export default function UseHook() {
   const [text, setText] = useState("");
 
   const ref = useRef(text);
+
+  const inputRef = useRef(null);
 
   function handleSend() {
     setTimeout(() => {
@@ -21,6 +39,7 @@ export default function UseHook() {
         }}
       />
       <button onClick={handleSend}>Send</button>
+      <NestedComponent ref={inputRef} />
     </>
   );
 }
