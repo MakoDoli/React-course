eeebc9ad3ad498a03c66e8ce900e45dc78b8cd54
MakoDoli <makodolidze@hotmail.com>
2024-02-05 01:37:13 +0400

    compound pattern

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 
 import LifeCycle from "./intermediate/theory/LifeCycle";
 import ButtonSum from "./homework/ButtonsSum";
+import { FlyOut } from "./intermediate/design patterns/Compound";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <ButtonSum />
       <hr></hr>
       <LifeCycle />
+      <FlyOut.Input />
     </div>
   );
 }
