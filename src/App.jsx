bed55fb4ce96b34c17a03c87a27ba8b8bacff479
MakoDoli <makodolidze@hotmail.com>
2024-02-04 21:43:18 +0400

    HOC pattern

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 
 import LifeCycle from "./intermediate/theory/LifeCycle";
 import ButtonSum from "./homework/ButtonsSum";
-import { TextWithStyle } from "./intermediate/design patterns/HOC";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <ButtonSum />
       <hr></hr>
       <LifeCycle />
-      <TextWithStyle />
     </div>
   );
 }
--- src/intermediate/design patterns/HOC.jsx
+++ src/intermediate/design patterns/HOC.jsx
@@ -1,3 +1,30 @@
+export function Wrapper(Component) {
+  // eslint-disable-next-line react/display-name
+  return (props) => {
+    const addText = "added text here";
+    return <Component {...props} addText={addText} />;
+  };
+}
+
+export function TextComponent() {
+  return <WrappedText text="from parent" />;
+}
+
+function Text({ text, addText }) {
+  return (
+    <p>
+      {text} {addText}
+    </p>
+  );
+}
+
+const WrappedText = Wrapper(Text);
+
+//************************* */
+//
+//
+// wrapper HOC takes Component as argument, and return inner arrow function, which has (props) parameter, which later will be passed to component, and creates new additional props assigned to const, and then returns component from argument which has  {...props} from parent and newProps={newProps} from wrapper <Component {...props} newProps={newProps} />
+
 // export function TextWithStyle() {
 //   return <StyledText text="inherited legacy" />;
 // }
